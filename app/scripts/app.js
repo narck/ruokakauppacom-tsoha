dee10294eac1f50667352028a8562193ab9bd831
Nicolas Arkkila <nicolas@arkkila.fi>
2014-06-06 18:50:38 +0300

    Cool animations

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,7 +4,8 @@ angular.module('ruokakauppacomTsohaApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ngRoute'
+  'ngRoute',
+  'ngAnimate'
 ])
   .config(function ($routeProvider, $locationProvider, $httpProvider) {
     $routeProvider
